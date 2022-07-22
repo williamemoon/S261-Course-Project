1302714ed4129ce4f77c551fe721fc1458333d05
williamemoon <williamemoon@gmail.com>
2022-07-21 21:34:25 -0400

    added loop began count

--- S261_Course_Project.py
+++ S261_Course_Project.py
@@ -1,12 +1,11 @@
-
-
+ 
 #function input employee information
 def employee_info():
-    employee_name = input("Enter the employee's name:   ")
+    employee_name = input("Enter the employee's name :   ")
     hours = float(input ("Enter the number of hours worked:  "))
     payrate = float(input("Enter the employee's payrate in dollars per hour:  "))
     taxrate = float(input ("Enter the income tax rate for this employee:  "))
-
+   
     return employee_name,  hours, payrate, taxrate
 
 #function gross pay
@@ -21,23 +20,25 @@ def netpay(taxrate, payrate, hours):
 def tax_ammount(taxrate, payrate, hours):
     return taxrate * hours * payrate
 
-#main
-if __name__ == "__main__":
-    employee_name, hours, payrate, taxrate  = employee_info()
-    print ("Name:             ", employee_name)
-    print ("Hours Worked:     ", hours, "Hours")
-    print ("Payrate:         $", payrate, "per Hour")
-    print ("Income Tax Rate:  ", taxrate, "%")
-    print ("Gross Pay:       $",grosspay(hours, payrate))
-    print ("Taxes Withheld:  $",tax_ammount(taxrate, payrate, hours))
-    print ("Net Pay:         $",netpay(taxrate, payrate, hours))
+next_employee = " "
+while (next_employee.lower() != "end"):
+    
+    #main
+    if __name__ == "__main__":
+        employee_name, hours, payrate, taxrate  = employee_info()
+        print ("Name:             ", employee_name)
+        print ("Hours Worked:     ", hours, "Hours")
+        print ("Payrate:         $", payrate, "per Hour")
+        print ("Income Tax Rate:  ", taxrate, "%")
+        print ("Gross Pay:       $",grosspay(hours, payrate))
+        print ("Taxes Withheld:  $",tax_ammount(taxrate, payrate, hours))
+        print ("Net Pay:         $",netpay(taxrate, payrate, hours))
+            
+    next_employee = input ("Would you like to enter another employee?  To end type \"end\":   ")
+    
+    count = 1
 
-
-    #Create a loop that will get user input by calling various functions until terminated by the user typing “End.”
-    #Create a function that will display total number of employees, total hours, total tax, and total net pay.
-    #Submit the Python source code file and a Word document that contains a screenshot of input and display for one employee and a screenshot of the display of totals. A minimum of five employees must be entered to receive full credit. Include a 1–2 sentence reflection on the successes and challenges you had with this assignment.
-
-
-
-
-
+    if next_employee != "end": 
+        count += 1        
+    else: 
+         print ("employee count = ", count)
